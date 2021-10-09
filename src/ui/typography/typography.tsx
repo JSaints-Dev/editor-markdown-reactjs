3e0df142f1b2ba634592f0fbaf495ba7b54a393f
Jean Saints <saintsjean.dev@gmail.com>
2021-10-09 19:16:04 -0300

    feat: added prop to weight in css the typography

--- src/ui/typography/typography.tsx
+++ src/ui/typography/typography.tsx
@@ -5,15 +5,18 @@ type typeTypography = {
   color?: keyof typeof theme.colors;
   fontSize?: keyof typeof theme.fonts.fontSizes;
   fontFamily?: keyof typeof theme.fonts.fontFamily;
+  fontWeight?: 400 | 700 | 'normal';
   variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'strong';
 };
 
 export const Typography = styled.span.attrs<typeTypography>(({ variant }) => ({
   as: variant,
 }))<typeTypography>`
-  ${({ color = 'black', fontSize = 'xs', fontFamily = 'primary' }) => css`
+  ${({ color = 'black', fontSize = 'xs', fontFamily = 'primary', fontWeight = 'normal' }) => css`
     color: ${theme.colors[color]};
     font-size: ${theme.fonts.fontSizes[fontSize]};
     font-family: ${theme.fonts.fontFamily[fontFamily]}, sans-serif;
+    font-weight: ${fontWeight};
+    margin: 0;
 `}
 `
